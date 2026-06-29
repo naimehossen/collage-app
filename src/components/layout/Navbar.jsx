b7d781985d5db52769034d7d0fe6ac21052db5ce
Md Naime Hossen <n33937407@gmail.com>
2026-06-29 23:57:41 +0600

    Update Navbar.jsx

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -153,7 +153,12 @@ const Navbar = () => {
               </a>
             </div>
             <button className="w-full bg-gradient-to-r from-blue-900 to-indigo-950 text-white py-4 rounded-2xl font-bold text-sm shadow-xl hover:shadow-blue-900/20 hover:-translate-y-0.5 transition-all active:scale-95">
-              <a href="https://collage-app-mxbt.onrender.com/admission" className="">অনলাইন অ্যাডমিশন</a>
+              <NavLink
+  to="/admission"
+
+>
+  অনলাইন অ্যাডমিশন
+</NavLink>
             </button>
           </div>
         </div>
